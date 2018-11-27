681a0ddddabe1e47d2689cf03b5ac41339fd1aff
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2018-11-27 12:01:27 -0500

    [ FIXD ] res and req

--- template/server/preLoadState.js
+++ template/server/preLoadState.js
@@ -1,12 +1,12 @@
 import counterDuck from 'reducers/counter'
  
-export default async function(res, req, next) {
-     // Get store from locals
-     const { store } = res.locals
-     // Dispatch a action to change initial state
-     await store.dispatch(counterDuck.creators.addCountFromServer())
-     // Resave new store
-     res.locals.store = store
-     // Pass middlerware
-     next()
+export default async function(req, res, next) {
+  // Get store from locals
+  const { store } = res.locals
+  // Dispatch a action to change initial state
+  await store.dispatch(counterDuck.creators.addCountFromServer())
+  // Resave new store
+  res.locals.store = store
+  // Pass middlerware
+  next()
 }
