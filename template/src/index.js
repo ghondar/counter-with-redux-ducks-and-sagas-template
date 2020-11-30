92644eff2af1e04ed7fbcc31f1c0d98818e37875
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2020-11-29 20:22:03 -0500

    [ REMOVE ] react-hot-loader

--- template/src/index.js
+++ template/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
 import { loadableReady } from '@loadable/component'
 
 import App from './App'
@@ -8,10 +7,7 @@ import App from './App'
 if(module.hot) module.hot.accept()
 
 const render = (Component, type = 'render') => {
-  ReactDOM[type](
-    <AppContainer key={Math.random()}>
-      <Component />
-    </AppContainer>,
+  ReactDOM[type](<Component />,
     document.getElementById('root')
   )
 }
@@ -21,9 +17,3 @@ if(process.env.NODE_ENV === 'production')
     render(App, 'hydrate')
   })
 else render(App)
-
-// Webpack Hot Module Replacement API
-if(module.hot)
-  module.hot.accept('./App', () => {
-    render(require('./App').default)
-  })
